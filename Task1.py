dc8b9eeeb2f288b0dacfa83c53ae63b7044557da
Anastasiya <a.tushina@rambler.ru>
2023-01-15 23:30:08 +0300

    ADD Tasks

---
+++ Task1.py
@@ -0,0 +1,6 @@
+#Напишите программу, удаляющую из текста все слова, содержащие ""абв"".
+
+my_str = 'АБВ ылоажы фыыдлх абв Зщышф вабвв ффлжв абВ'
+new_str = ' '.join(list(filter(lambda elem: 'абв' not in elem.lower(), my_str.split())))
+print(my_str)
+print(new_str)
